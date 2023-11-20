c15ef448ec395285b6906718db984a02eade9655
James Kang <79829085+Si-Hyeak-KANG@users.noreply.github.com>
2023-11-20 09:51:51 +0900

    Create 평균 일일 대여 요금 구하기_시혁.sql

---
+++ 고득점KIT/sql/평균 일일 대여 요금 구하기_시혁.sql
@@ -0,0 +1,9 @@
+SELECT ROUND(AVG(DAILY_FEE), 0) AS AVERAGE_FEE
+FROM CAR_RENTAL_COMPANY_CAR
+WHERE CAR_TYPE = 'SUV';
+
+-- ROUND : 반올림
+-- ROUND(v, p) : 반올림 자릿수 설정. p가 자릿수
+-- CELI : 올림
+-- FLOOR : 소수 모두 버림
+-- TRUNCATE(v,p) : 버림 자릿수 설정. p가 자릿수
