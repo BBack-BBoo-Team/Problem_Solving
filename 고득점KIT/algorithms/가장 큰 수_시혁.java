2155968ad95ddb077082d61d8493737cb94eba5f
James Kang <79829085+Si-Hyeak-KANG@users.noreply.github.com>
2023-11-21 13:52:22 +0900

    Update 가장 큰 수_시혁.java

--- 고득점KIT/algorithms/가장 큰 수_시혁.java
+++ 고득점KIT/algorithms/가장 큰 수_시혁.java
@@ -8,12 +8,16 @@ import java.util.Arrays;
 class Solution {
     public String solution(int[] numbers) {
  
+        // 1. 정수 타입의 numbers를 문자열 배열에 저장 (Stream 활용)
         String[] numsToString = Arrays.stream(numbers)
                                         .mapToObj(String::valueOf)
                                         .toArray(String[]::new);
         
+        // 2. 직접 Custom 정렬 방식으로 정렬
         Arrays.sort(numsToString, (s1, s2) -> (s2+s1).compareTo(s1+s2));
         
+        // 3. String은 더 할때마다 메모리 소비가 있기 때문에
+        //    가변성을 가진 StringBuilder 활용
         StringBuilder sb = Arrays.stream(numsToString)
             .collect(StringBuilder::new, StringBuilder::append, StringBuilder::append);
         
