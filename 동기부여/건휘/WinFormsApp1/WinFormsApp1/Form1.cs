9b0ef4180ff8850636db40da49f90d8a8cea7bf1
kimjeju <kimjeju@peppercon.co.kr>
2023-11-23 00:38:28 +0900

    테스트

---
+++ 동기부여/건휘/WinFormsApp1/WinFormsApp1/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            Sonata myCar = new Sonata();         
+        }
+    }
+
+    class Cat
+    {
+        string Name;
+        int Age;
+    }
+
+    class Sonata
+    {
+        string Color;
+        bool HasSunRoof;
+    }
+}
