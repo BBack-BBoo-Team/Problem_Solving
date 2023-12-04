5e30f511ebf38d47d37a513e4a93218d43918ddc
kimjeju <kimjeju@peppercon.co.kr>
2023-12-04 23:26:36 +0900

    포트폴리오 레이아웃 조정

---
+++ 동기부여/건휘/kunhwi-folio/src/Sections/Intro/Intro.js
@@ -0,0 +1,18 @@
+import "../../Styles/intro.css"
+
+function Intro()
+{
+    return (
+        <div className="intro-body">
+            <div className="intro-left-body">
+                    ㅎㅇ
+            </div>
+            <div className="intro-right-body">
+                    ㅎㅇ
+
+            </div>
+        </div>
+    )
+}
+
+export default Intro
