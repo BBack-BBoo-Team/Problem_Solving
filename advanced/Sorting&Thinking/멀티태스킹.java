034cc3585cd1605271eed4bb36478c8496370f64
James Kang <79829085+Si-Hyeak-KANG@users.noreply.github.com>
2024-01-11 12:47:30 +0900

    Create 멀티태스킹.java

---
+++ advanced/Sorting&Thinking/멀티태스킹.java
@@ -0,0 +1,26 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class 멀티태스킹 {
+
+    // k초 전까지 작업 진행하고, 정전되었을 때 queue가 비었으면 -1, 아니면 제일 앞 작업의 인덱스 추출
+    public int solution(int[] tasks, long k) {
+        Queue<int[]> work = new LinkedList<>(); // 0번: 인덱스, 1번: 작업시간
+        for(int i = 0; i < tasks.length; i++) work.add(new int[]{i+1,tasks[i]});
+        while(k-->0 && !work.isEmpty()) {
+            int[] curr = work.poll();
+            if (curr[1]==1) continue;
+            work.add(new int[]{curr[0], curr[1]-1});
+        }
+
+        if(work.isEmpty()) return -1;
+        else return work.poll()[0];
+    }
+
+    public static void main(String[] args){
+        멀티태스킹 T = new 멀티태스킹();
+        System.out.println(T.solution(new int[]{1, 2, 3}, 5));
+        System.out.println(T.solution(new int[]{8, 5, 2, 9, 10, 7}, 30));
+        System.out.println(T.solution(new int[]{8, 9, 12, 23, 45, 16, 25, 50}, 100));
+    }
+}
