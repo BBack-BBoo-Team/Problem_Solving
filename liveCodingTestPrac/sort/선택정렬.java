6a415110ee587b6f740af58ec52f5a2ea7c82795
James Kang <79829085+Si-Hyeak-KANG@users.noreply.github.com>
2024-01-23 13:51:34 +0900

    Update 선택정렬.java

--- liveCodingTestPrac/sort/선택정렬.java
+++ liveCodingTestPrac/sort/선택정렬.java
@@ -1,1 +1,21 @@
-
+/**
+* 1) 배열의 인덱스만큼 순회한다.
+* 2) 주어진 배열에서 현재 인덱스부터 마지막 인덱스까지 한번 더 순회하며, 최솟값을 찾는다.
+* 3) 찾은 값을 현재 인덱스에 넣는다.
+* 4) 마지막인덱스는 자동 정렬이므로 탐색하지 않는다.
+* 시간복잡도 O(n^2)
+*/
+int[] selectSort(int arr[]) {
+  int n = arr.length;
+  int[] answer = new int[n];
+  for(int i = 0; i < n-1; i++) {
+    int min = Integer.MIN_VALUE;
+    for(int j = i+1; j < n; j++) {
+      int curr = arr[j];
+      if(min <= curr) continue;
+      min = curr;
+    }
+    answer[i] = min;
+  }
+  return answer;
+}
